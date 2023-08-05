50d1b8f1ce892179e254a8c1ca1132f068cd3464
Tyetian <59758284+Tyetian@users.noreply.github.com>
2023-08-05 23:18:34 +0800

    Update index.cjs
    update

--- config/index.cjs
+++ config/index.cjs
@@ -8,22 +8,22 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx00baa6d9acfa5210',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '06ef915800b38251c039e4c526c9231f',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '上海',
+  CITY: '闵行',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: 'tye',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'gh_3f79be0876d1',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'beAdzKe2cR7-_WfC3DDZVo9H527Gu3C3M4K_9lMUJgw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
