b94f652834677592af1adf89da5266f4ab12915c
Tyetian <59758284+Tyetian@users.noreply.github.com>
2023-08-06 20:17:50 +0800

    Update index.cjs
    update2

--- config/index.cjs
+++ config/index.cjs
@@ -19,9 +19,9 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '时迁',
+      name: 'Tzz',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o04pK62PQKxVM5OjImj39H74zihE',
+      id: 'o04pK6zjzEt31PQMq3VOsXPH-a2M',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'ENK8ScR5OO2vIWIRcbdG1AQY8Ef6imlNn8Gxgp1_1NQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
